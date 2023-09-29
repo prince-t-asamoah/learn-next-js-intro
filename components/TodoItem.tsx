ecb1d6f2f384663570b8a06e8d7774a19d87a385
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-09-29 12:24:45 +0000

    feat: add edit link and delete button

--- components/TodoItem.tsx
+++ components/TodoItem.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import Link from 'next/link';
+
 interface TodoItemProps {
     id: string;
     title: string;
@@ -14,20 +16,30 @@ export default function TodoItem({
     toggleTodo,
 }: TodoItemProps) {
     return (
-        <li className="text-white flex items-center gap-2">
-            <input
-                id={id}
-                type="checkbox"
-                className="cursor-pointer peer"
-                defaultChecked={complete}
-                onChange={(e) => toggleTodo(id, e.target.checked)}
-            />
-            <label
-                htmlFor={id}
-                className="peer-checked:line-through peer-checked:text-slate-400"
-            >
-                {title}
-            </label>
+        <li className="text-white py-2 flex items-center justify-between gap-2">
+            <div className="flex gap-2">
+                <input
+                    id={id}
+                    type="checkbox"
+                    className="cursor-pointer peer"
+                    defaultChecked={complete}
+                    onChange={(e) => toggleTodo(id, e.target.checked)}
+                />
+                <label
+                    htmlFor={id}
+                    className="peer-checked:line-through peer-checked:text-slate-400"
+                >
+                    {title}
+                </label>
+            </div>
+            <div className="flex gap-2 items-center">
+                <Link href="/edit" className="hover:underline">
+                    Edit
+                </Link>
+                <button className="hover:bg-red-400 p-1 rounded-md">
+                    Delete
+                </button>
+            </div>
         </li>
     );
 }
