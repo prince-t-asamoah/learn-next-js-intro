51be934a8269756a834eed659555e9b34ffacbf8
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-09-29 09:44:41 +0000

    style: change label color on checkbox checked

--- components/TodoItem.tsx
+++ components/TodoItem.tsx
@@ -6,9 +6,9 @@ interface TodoItemProps {
 
 export default function TodoItem({ id, title, complete }: TodoItemProps) {
     return (
-        <li>
+        <li className="text-white flex items-center gap-2">
             <input id={id} type="checkbox" className="cursor-pointer peer" />
-            <label htmlFor={id} className="peer-checked:line-through">
+            <label htmlFor={id} className="peer-checked:line-through peer-checked:text-slate-400">
                 {title}
             </label>
         </li>
