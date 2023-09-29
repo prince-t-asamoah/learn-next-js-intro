69d8fa8e36b55987d9d5b9557c733b024c148abb
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-09-29 09:42:57 +0000

    feat: create a new todo page

---
+++ app/new/page.tsx
@@ -0,0 +1,9 @@
+import Header from '@/components/Header';
+
+export default function NewTodo() {
+    return (
+        <div className="p-5">
+            <Header title="New Todo" />
+        </div>
+    );
+}
