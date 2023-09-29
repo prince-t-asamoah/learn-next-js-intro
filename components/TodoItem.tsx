797d0eac8e03461633a6277b58cebe64656b92fb
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-09-29 11:01:44 +0000

    feat: use toggleTodo on check input

--- components/TodoItem.tsx
+++ components/TodoItem.tsx
@@ -1,14 +1,31 @@
+'use client';
+
 interface TodoItemProps {
     id: string;
     title: string;
     complete: boolean;
+    toggleTodo: (id: string, complete: boolean) => void;
 }
 
-export default function TodoItem({ id, title, complete }: TodoItemProps) {
+export default function TodoItem({
+    id,
+    title,
+    complete,
+    toggleTodo,
+}: TodoItemProps) {
     return (
         <li className="text-white flex items-center gap-2">
-            <input id={id} type="checkbox" className="cursor-pointer peer" />
-            <label htmlFor={id} className="peer-checked:line-through peer-checked:text-slate-400">
+            <input
+                id={id}
+                type="checkbox"
+                className="cursor-pointer peer"
+                defaultChecked={complete}
+                onChange={(e) => toggleTodo(id, e.target.checked)}
+            />
+            <label
+                htmlFor={id}
+                className="peer-checked:line-through peer-checked:text-slate-400"
+            >
                 {title}
             </label>
         </li>
