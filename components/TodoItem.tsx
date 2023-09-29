a64b38d55abfa84be383b757bd17a35103ab11fa
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-09-29 13:00:00 +0000

    style: update list text and background color

--- components/TodoItem.tsx
+++ components/TodoItem.tsx
@@ -16,18 +16,18 @@ export default function TodoItem({
     toggleTodo,
 }: TodoItemProps) {
     return (
-        <li className="text-white bg-slate-100 py-2 px-4 flex items-center justify-between gap-2">
-            <div className="flex gap-2">
+        <li className="bg-slate-100 py-2 px-4 flex items-center justify-between gap-2 border-b">
+            <div className="flex items-center gap-2">
                 <input
                     id={id}
                     type="checkbox"
-                    className="cursor-pointer peer"
+                    className="cursor-pointer peer w-4 h-4 rounded focus:ring-2 checked:ring-blue-500"
                     defaultChecked={complete}
                     onChange={(e) => toggleTodo(id, e.target.checked)}
                 />
                 <label
                     htmlFor={id}
-                    className="peer-checked:line-through peer-checked:text-slate-400"
+                    className="text-slate-600 peer-checked:line-through peer-checked:text-slate-400"
                 >
                     {title}
                 </label>
