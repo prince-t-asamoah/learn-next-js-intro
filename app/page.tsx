9cc4ea6637d823c0efe545e8f80e8e0ad03c2765
PRINCE ASAMOAH <prince.asamoah@amalitech.com>
2023-09-29 09:44:12 +0000

    feat: use page header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,21 @@
 import Link from 'next/link';
 import { prisma } from './db';
 import TodoItem from '@/components/TodoItem';
+import Header from '@/components/Header';
 
 export default async function Home() {
     const todos = await prisma.todo.findMany();
 
     return (
         <main className="h-screen p-5">
-            <header className="text-white flex justify-between items-center mb-4">
-                <h1 className="text-2xl">Todos</h1>
+            <Header title="New">
                 <Link
-                    href="/"
+                    href="/new"
                     className="outline outline-1 px-2 py-1 rounded-sm hover:bg-slate-100 hover:text-slate-800"
                 >
                     New
                 </Link>
-            </header>
+            </Header>
             <ul className="pl-4">
                 {todos.map((todo) => (
                     <TodoItem key={todo.id} {...todo} />
